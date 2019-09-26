487fbb491d3194609c6fe9964dc54c6403c7f62e
Adam Tistler <adam.tistler@rackspace.com>
2019-09-25 21:14:03 -0400

    fix(dex): fix provider reference

--- packages/eks/src/dex.ts
+++ packages/eks/src/dex.ts
@@ -1,6 +1,5 @@
 import * as _ from "lodash";
 import * as base from '@k8s-ms-modules-demo/common';
-import * as eks from '@pulumi/eks';
 import * as k8s from '@pulumi/kubernetes';
 import {ComponentResourceOptions} from "@pulumi/pulumi";
 
@@ -17,8 +16,8 @@ class DexChart extends base.DexChart {
 export class Dex extends base.Dex {
     readonly chartAdapter: DexChart;
 
-    constructor(cluster: eks.Cluster, namespace: k8s.core.v1.Namespace, chartOptions: base.ChartOptions, opts?: ComponentResourceOptions) {
-        super(cluster.provider, namespace, chartOptions, opts);
-        this.chartAdapter = new DexChart('dex', cluster.provider, namespace, chartOptions);
+    constructor(provider: k8s.Provider, namespace: k8s.core.v1.Namespace, chartOptions: base.ChartOptions, opts?: ComponentResourceOptions) {
+        super(provider, namespace, chartOptions, opts);
+        this.chartAdapter = new DexChart('dex', provider, namespace, chartOptions);
     }
 }
