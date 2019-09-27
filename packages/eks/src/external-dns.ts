096a5128dad9f1c5be7b44e156b3ddad7cbd8046
Adam Tistler <adam.tistler@rackspace.com>
2019-09-27 09:55:32 -0400

    fix(test): test

--- packages/eks/src/external-dns.ts
+++ packages/eks/src/external-dns.ts
@@ -9,7 +9,6 @@ class ExternalDnsChart extends base.ExternalDnsChart {
     configNamespace(): string {
         return 'ManagedServices/ExternalDns/Chart'
     }
-
     defaults(): object {
         return _.merge(super.defaults(), {
             provider: 'aws',
